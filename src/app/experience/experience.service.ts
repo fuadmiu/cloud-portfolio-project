8fd1aae2d0b432c7894350483b1a832612212481
Mohammad Fuad <fuad@Mohammads-Mac-mini.local>
2024-05-22 22:15:54 -0500

    update

--- src/app/experience/experience.service.ts
+++ src/app/experience/experience.service.ts
@@ -7,22 +7,25 @@ import { experience } from './model/experience';
 export class ExperienceService {
 
   experienceList:experience[]=[
-    new experience("March, 2021","January 2023","Senior Software Engineer/Team Lead","Kaz Software",
-    "Dhaka, Bangaladesh","Analyzed, designed and developed a project framework from a legacy Java monolith application into an ASP.NET application using microservices architecture."+
-    " Updated, modified, and expanded existing web applications using C#, ASP.NET MVC, and Azure DevOps."+
-    " Planned sprints and conducted day-to-day scrum meetings as a scrum master."),
-    new experience("October 2019","February 2021","Assistant Manager",
-    "MISAMI-BITPI Group","Dhaka, Bangladesh","Enhanced, and modified existing enterprise level web and desktop-based applications using C#, ASP.NET Core, and ASP.NET MVC."+
-    " Wrote stored procedures, user define functions, views, triggers, and complex SQL Queries in SQL Server"),
-    new experience("October 2017","September 2019","Senior Programmer",
-    "MISAMI-BITPI Group","Dhaka, Bangladesh"),
-    new experience("September 2016","September 2017","Programmer",
-    "MISAMI-BITPI Group","Dhaka, Bangladesh"),
-    new experience("May 2013","August 2016","Software Engineer","Global Software Architects",
-    "Dhaka, Bangladesh","Wrote server-side code using ASP.NET MVC and ASP.NET Web Forms."+
-    " Wrote store procedures and triggers using MS SQL Server."+
-    " Worked intensely on the front-end using jQuery, bootstrap, and the Google Maps API."+
-    " Developed and maintained mobile applications using Xamarin for Android and iOS.")
+    new experience("October 2023","Present","Full Stack Engineer","Creospan Inc",
+    "Chicago, Illinois, USA","Working as a Full Stack Java/React developer for the client Northern Trust."+
+    " Implemented event driven architecture using Kafka. Practiced TDD and best software engineering practices."+
+    " Developed front-end application using React.js"),
+    new experience("October 2018","March 2022","Senior Software Engineer",
+    "KAZ Software Ltd","Dhaka, Bangladesh","Managed software engineering team by following agile methodologies while delivering software solutions according to client requirements."+
+    " Assisted in architecting, developing, analyzing, and deploying business-critical applications." +
+    " Increased application performance by migrating monolith to Microservice-based solution." +
+    " Developed projects using React.js, Angular with Typescript and JavaScript."),
+    new experience("October 2016","September 2018","Software Engineer",
+    "KAZ Software Ltd","Dhaka, Bangladesh", "Played key role in numerous projects by engaging in all stages of the development cycle."+
+    " Enhanced existing & developed new applications based on client feedback." +
+    " Implemented code reviews of other developers to maximize code quality." +
+    " Designed backend and frontend systems."),
+    new experience("June 2013","September 2016","Associate Software Engineer",
+    "KAZ Software Ltd","Dhaka, Bangladesh","Analyzed, implemented, and deployed successful software projects."+
+    " Designed database system for applications." +
+    " Improved application performance 20% by optimizing existing database queries." +
+    " Developed Java-based driver applications to convert XML files to Word file.")
   ];
     constructor() { 
   
